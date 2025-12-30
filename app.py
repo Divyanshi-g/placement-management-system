ec9ea9c3d418bc6723889c95602085defd447f79
Divyanshi-g <divyanshig087@gmail.com>
2025-12-30 14:34:37 +0530

    app.py

--- app.py
+++ app.py
@@ -48,8 +48,9 @@ def create_app():
 app = create_app()
 if __name__ == "__main__":
     app.run(
-        debug=True,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+       debug = os.environ.get("FLASK_ENV") == "development"
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=debug)
     )
+
 
