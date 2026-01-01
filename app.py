add7a00a6d10a1386a6723aa01098788557aac90
Divyanshi-g <divyanshig087@gmail.com>
2026-01-01 19:57:47 +0530

    app.py

--- app.py
+++ app.py
@@ -41,13 +41,14 @@ def create_app():
     # ---------------- Routes ----------------
     @app.route("/")
     def index():
-        return render_template("base.html")
+        return render_template("home.html")
 
     return app
 
 app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
