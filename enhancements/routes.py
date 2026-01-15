0dc28290005dc404f4f323a410e2e080ab9e0995
Divyanshi-g <divyanshig087@gmail.com>
2026-01-15 20:53:01 +0530

    routes.py

--- enhancements/routes.py
+++ enhancements/routes.py
@@ -4,6 +4,7 @@ import os
 import json
 import csv
 import sqlite3
+import request
 from datetime import datetime
 from openai import OpenAI
 
@@ -1409,6 +1410,7 @@ def check_resume():
     except Exception as e:
         print("❌ Error in check_resume:", str(e))
         return jsonify({"error": str(e)}), 500
+
 
 
 
