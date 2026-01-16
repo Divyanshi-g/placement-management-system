e6c9881d5a3920b1929668735515ef4a09f2f40d
Divyanshi-g <divyanshig087@gmail.com>
2026-01-16 23:29:37 +0530

    routes.py

--- enhancements/routes.py
+++ enhancements/routes.py
@@ -570,12 +570,12 @@ def apply(placement_id):
             student_name, phone, course,
             skills, experience, resume, status
         )
-        VALUES (?, ?, ?, ?, ?, ?, ?, 'Applied')
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'Applied')
     """, (
         user_id, placement_id,
         student_name, phone, course,
-        skills, experience, resume_filename)
-    )
+        skills, experience, resume_filename, status
+    ))
 
     conn.commit()
     conn.close()
@@ -1460,6 +1460,7 @@ def admin_questions1_message():
     return jsonify({"reply": reply})
 
     
+
 
 
 
