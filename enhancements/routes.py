e8206f4b12fe762242cc6e6c99b31afaebc101f2
Divyanshi-g <divyanshig087@gmail.com>
2026-01-16 23:05:08 +0530

    routes.py

--- enhancements/routes.py
+++ enhancements/routes.py
@@ -571,7 +571,7 @@ def apply(placement_id):
     conn.close()
 
     flash("✅ Application submitted successfully!")
-    return redirect(url_for("enhancements.student_dashboard"))
+    return redirect(url_for("enhancements.placements.html"))
 
 # ------------------ Profile ------------------
 @enhancements_bp.route("/profile", methods=["GET", "POST"])
@@ -1454,6 +1454,7 @@ def admin_questions1_message():
     return jsonify({"reply": reply})
 
     
+
 
 
 
