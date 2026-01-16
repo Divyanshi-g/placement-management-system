b748eac5912767cab4ecbcd3d395e0ea7e819c92
Divyanshi-g <divyanshig087@gmail.com>
2026-01-16 12:33:23 +0530

    db.py

--- enhancements/db.py
+++ enhancements/db.py
@@ -7,7 +7,7 @@ from flask import g, current_app
 # -------------------------------------------------
 def get_db_conn():
     if "db_conn" not in g:
-        db_path = current_app.config.get("DATABASE", "placement.db")
+        db_path = current_app.config.get("DATABASE", "placements.db")
 
         # store DB inside instance folder if relative
         if not os.path.isabs(db_path):
@@ -236,6 +236,7 @@ def init_db():
         ])
 
     db.commit()
+
 
 
 
